a1af92e076c87b80a6cb76fd1865c9568038fdc5
Adriano Santos <adrianodrix@gmail.com>
2021-02-21 18:25:38 -0300

    feat: add logMongoRepo to SignUp Factory

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,9 +3,9 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
-import { Controller } from '@/presentation/protocols'
+import { Controller } from '../..//presentation/protocols'
 import { LogControllerDecorator } from '../decorators/log'
-import { LogMongoRepository } from '@/infra/db/mongodb/log-repository/log-mongo-repository'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log-mongo-repository'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
