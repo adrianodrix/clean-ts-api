0657f2e5ab83f004668901ad6a0aa9c67f506812
Adriano Santos <adrianodrix@gmail.com>
2021-02-21 14:58:25 -0300

    feat: add signup routes

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
 import securityPolicy from './security-policy'
+import setupRoutes from './routes'
 
 const app = express()
 
 securityPolicy(app)
 setupMiddlewares(app)
+setupRoutes(app)
 
 export default app
