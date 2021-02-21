d39cfcf6f19a1a70a2958e4e802b2d95b62613c6
Adriano Santos <adrianodrix@gmail.com>
2021-02-21 14:26:38 -0300

    feat: ensure cors is enabled

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,10 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
+import securityPolicy from './security-policy'
 
 const app = express()
+
+securityPolicy(app)
 setupMiddlewares(app)
+
 export default app
