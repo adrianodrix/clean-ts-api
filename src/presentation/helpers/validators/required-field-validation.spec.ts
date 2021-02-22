117f012a5d99bdfaa8f04298923bfbfeb820ea74
Adriano Santos <adrianodrix@gmail.com>
2021-02-22 15:45:37 -0300

    refactor: creation sut factory

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,15 +1,17 @@
 import { MissingParamError } from '@/presentation/errors'
 import { RequiredFieldValidation } from './required-fields-validation'
 
+const makeSut = (): RequiredFieldValidation => (new RequiredFieldValidation('any_field'))
+
 describe('RequiredField Validation', () => {
   test('should return a MissinParamError if validation fails', () => {
-    const sut = new RequiredFieldValidation('any_field')
+    const sut = makeSut()
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('any_field'))
   })
 
   test('should not return if validation succeeds', () => {
-    const sut = new RequiredFieldValidation('any_field')
+    const sut = makeSut()
     const error = sut.validate({ any_field: 'any_value' })
     expect(error).toBeFalsy()
   })
