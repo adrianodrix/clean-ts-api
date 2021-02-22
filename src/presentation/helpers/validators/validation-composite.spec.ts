47ae4300d689c65d802513943a532dc7b4ce97c2
Adriano Santos <adrianodrix@gmail.com>
2021-02-22 17:34:59 -0300

    refactor: creation sut factory

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -4,18 +4,22 @@ import { ValidationComposite } from './validation-composite'
 
 class ValidationStub implements Validation {
   validate (input: any): Error {
-    return new MissingParamError('field')
+    return null as any
   }
 }
 
-const makeSut = (): ValidationComposite => {
+const makeSut = (): any => {
   const validationStub = new ValidationStub()
-  return new ValidationComposite([validationStub])
+  return {
+    sut: new ValidationComposite([validationStub]),
+    validationStub
+  }
 }
 
 describe('Validation Composite', () => {
   test('should return an error if any validation fails', () => {
-    const sut = makeSut()
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('field'))
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new MissingParamError('field'))
   })
