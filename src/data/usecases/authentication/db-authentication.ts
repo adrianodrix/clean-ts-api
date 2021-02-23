9fad50343a135523564e034cd05e05f01008c5c3
Adriano Santos <adrianodrix@gmail.com>
2021-02-23 13:27:37 -0300

    feat: should call TokenGenerator with correct id

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,14 +1,17 @@
 import { HashComparer } from '@/data/protocols/criptography/hash-comparer'
+import { TokenGenerator } from '@/data/protocols/criptography/token-generator'
 import { LoadAccountByEmailRepo } from '@/data/protocols/db/load-account-by-email-repo'
 import { Authentication, AuthenticationModel } from '@/domain/usecases/authentication'
 
 export class DbAuhentication implements Authentication {
   private readonly loadAccountByEmailRepo: LoadAccountByEmailRepo
   private readonly hashComparer: HashComparer
+  private readonly tokenGenerator: TokenGenerator
 
-  constructor (loadAccountByEmailRepo: LoadAccountByEmailRepo, hashComparer: HashComparer) {
+  constructor (loadAccountByEmailRepo: LoadAccountByEmailRepo, hashComparer: HashComparer, tokenGenerator: TokenGenerator) {
     this.loadAccountByEmailRepo = loadAccountByEmailRepo
     this.hashComparer = hashComparer
+    this.tokenGenerator = tokenGenerator
   }
 
   async auth (authentication: AuthenticationModel): Promise<string> {
@@ -16,6 +19,7 @@ export class DbAuhentication implements Authentication {
     const account = await this.loadAccountByEmailRepo.load(email)
     if (account) {
       await this.hashComparer.comparer(password, account.password)
+      await this.tokenGenerator.generate(account.id)
     }
 
     return null as any
