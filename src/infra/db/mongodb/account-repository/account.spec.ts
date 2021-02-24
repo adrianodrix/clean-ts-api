a4b3b314034a52498316ec5414b6ee0c372109e9
Adriano Santos <adrianodrix@gmail.com>
2021-02-24 14:58:47 -0300

    test: should return an account on load by email success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,5 +1,10 @@
+import { AccountModel } from '@/domain/models/account'
+import { AddAccountModel } from '@/domain/usecases/add-account'
+import { Collection } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account'
+
+let accountCollection: Collection
 
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
@@ -11,7 +16,7 @@ describe('Account Mongo Repository', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -19,13 +24,26 @@ describe('Account Mongo Repository', () => {
     return new AccountMongoRepository()
   }
 
-  test('should return an account on success', async () => {
+  const makeFakeAccount = (): AddAccountModel => ({
+    name: 'valid_name',
+    email: 'valid_email@mail.com',
+    password: 'valid_password'
+  })
+
+  test('should return an account on add success', async () => {
     const sut = makeSut()
-    const account = await sut.add({
-      name: 'valid_name',
-      email: 'valid_email@mail.com',
-      password: 'valid_password'
-    })
+    const account = await sut.add(makeFakeAccount())
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('valid_name')
+    expect(account.email).toBe('valid_email@mail.com')
+  })
+
+  test('should return an account on load by email success', async () => {
+    const sut = makeSut()
+    await accountCollection.insertOne(makeFakeAccount())
+    const account = await sut.loadByEmail('valid_email@mail.com')
+
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
     expect(account.name).toBe('valid_name')
