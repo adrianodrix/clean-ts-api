6f192aa15f9566a4aa089b3704936927487bebf7
Adriano Santos <adrianodrix@gmail.com>
2021-02-25 04:59:18 -0300

    refactor: run debug with watch build

--- src/main/server.ts
+++ src/main/server.ts
@@ -6,6 +6,6 @@ console.log(env.mongoUrl)
 mongodb.connect(env.mongoUrl)
   .then(async () => {
     const app = (await import('./config/app')).default
-    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+    app.listen(env.port, () => console.log(`Server running at .. http://localhost:${env.port}`))
   })
   .catch(console.error)
