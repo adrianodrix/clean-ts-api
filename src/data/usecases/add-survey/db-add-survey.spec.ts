e1aa4ac587066669b6bc28532cdbf220aedf3d0a
Adriano Santos <adrianodrix@gmail.com>
2021-02-28 23:40:50 -0300

    feat: ensure DbSurvey calls

---
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -0,0 +1,45 @@
+import { AddSurveyRepository } from '@/data/protocols/db/survey/add-survey-repository'
+import { AddSurveyModel } from '@/domain/usecases/add-survey'
+import { DbAddSurvey } from './db-add-survey'
+
+class AddSurveyRepositoryStub implements AddSurveyRepository {
+  async add (surveyData: AddSurveyModel): Promise<void> {}
+}
+
+interface SutTypes {
+  sut: DbAddSurvey
+  addSurveyRepositoryStub: AddSurveyRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addSurveyRepositoryStub = new AddSurveyRepositoryStub()
+  const sut = new DbAddSurvey(addSurveyRepositoryStub)
+  return {
+    sut,
+    addSurveyRepositoryStub
+  }
+}
+
+const makeFakeData = (): AddSurveyModel => ({
+  question: 'any_question',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer'
+  }]
+})
+
+describe('DBAddSurvey UseCase', () => {
+  test('should call AddSurveyRepository with correct values', async () => {
+    const { sut, addSurveyRepositoryStub } = makeSut()
+    const addSurveyRepositorySpy = jest.spyOn(addSurveyRepositoryStub, 'add')
+    await sut.add(makeFakeData())
+    expect(addSurveyRepositorySpy).toHaveBeenCalledWith(makeFakeData())
+  })
+
+  test('should throw if AddSurveyRepository throws', async () => {
+    const { sut, addSurveyRepositoryStub } = makeSut()
+    jest.spyOn(addSurveyRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.add(makeFakeData())
+    await expect(promise).rejects.toThrow()
+  })
+})
