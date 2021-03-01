ec86b0b86429907c634053b592baf5e22e3ad04e
Adriano Santos <adrianodrix@gmail.com>
2021-03-01 00:30:32 -0300

    feat: should return 204 and a token on signup

---
+++ src/main/routes/survey/survey-routes.spec.ts
@@ -0,0 +1,39 @@
+import request from 'supertest'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import app from '@/main/config/app'
+import { Collection } from 'mongodb'
+
+let surveyCollection: Collection
+
+beforeAll(async () => {
+  await MongoHelper.connect(process.env.MONGO_URL)
+})
+
+afterAll(async () => {
+  await MongoHelper.disconnect()
+})
+
+beforeEach(async () => {
+  surveyCollection = await MongoHelper.getCollection('surveys')
+  await surveyCollection.deleteMany({})
+})
+
+describe('POST /surveys', () => {
+  test('should return 204 when add a survey', async () => {
+    await request(app)
+      .post('/api/surveys')
+      .send({
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }, {
+          answer: 'other_answer'
+        }]
+      })
+      .expect(204)
+
+    const surveyResponse = await surveyCollection.findOne({ question: 'any_question' })
+    expect(surveyResponse).toBeTruthy()
+  })
+})
---
+++ src/main/routes/survey/survey-routes.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+import { adaptRoute } from '@/main/adapters/express/express-routes-adapter'
+import { makeSurveyController } from '@/main/factories/controllers/survey/add-survey-controller-factory'
+
+export default (router: Router): void => {
+  router.post('/surveys', adaptRoute(makeSurveyController()))
+}
