f3954bd493361689c5246804bfebeb8790b9fb95
Adriano Santos <adrianodrix@gmail.com>
2021-03-01 21:23:26 -0300

    feat: ensure return 200 when list of the surveys with valid access token

--- src/main/routes/survey/survey-routes.test.ts
+++ src/main/routes/survey/survey-routes.test.ts
@@ -6,6 +6,7 @@ import { Collection } from 'mongodb'
 import { hash } from 'bcrypt'
 import { sign } from 'jsonwebtoken'
 import env from '@/main/config/env'
+import { SurveyModel } from '@/domain/models/survey'
 
 let surveyCollection, accountCollection: Collection
 
@@ -27,6 +28,58 @@ beforeEach(async () => {
   await accountCollection.deleteMany({})
 })
 
+const makeFakeSurveys = (): SurveyModel[] => {
+  return [
+    {
+      id: 'any_id',
+      question: 'any_question',
+      date: new Date(),
+      answers: [
+        {
+          image: 'any_image',
+          answer: 'any_ansser'
+        }
+      ]
+    },
+    {
+      id: 'other_id',
+      question: 'other_question',
+      date: new Date(),
+      answers: [
+        {
+          image: 'other_image',
+          answer: 'other_ansser'
+        },
+        {
+          answer: 'one_more_ansser'
+        }
+      ]
+    }
+  ]
+}
+
+const makeFakeAccount = async (): Promise<string> => {
+  const password = await hash('123', 12)
+  const res = await accountCollection.insertOne({
+    name: 'Adriano Santos',
+    email: 'adrianodrix@gmail.com',
+    role: 'admin',
+    password
+  })
+
+  const id = res.ops[0]._id
+  const accessToken = sign({ id }, env.jwtSecret)
+
+  await accountCollection.updateOne({
+    _id: id
+  }, {
+    $set: {
+      accessToken
+    }
+  })
+
+  return accessToken
+}
 describe('POST /surveys', () => {
   test('should return 403 when add a survey whithout accessToken', async () => {
     await request(app)
@@ -43,24 +96,7 @@ describe('POST /surveys', () => {
   })
 
   test('should return 204 when add a survey with valid access token', async () => {
-    const password = await hash('123', 12)
-    const res = await accountCollection.insertOne({
-      name: 'Adriano Santos',
-      email: 'adrianodrix@gmail.com',
-      role: 'admin',
-      password
-    })
-
-    const id = res.ops[0]._id
-    const accessToken = sign({ id }, env.jwtSecret)
-
-    await accountCollection.updateOne({
-      _id: id
-    }, {
-      $set: {
-        accessToken
-      }
-    })
+    const accessToken = await makeFakeAccount()
 
     await request(app)
       .post('/api/surveys')
@@ -87,5 +123,30 @@ describe('GET /surveys', () => {
     await request(app)
       .get('/api/surveys')
       .expect(403)
+  })
+
+  test('should return 403, when load all surveys with invalid access token', async () => {
+    const accessToken = sign({ id: 'invalid_token' }, env.jwtSecret)
+    await request(app)
+      .post('/api/surveys')
+      .set('x-access-token', accessToken)
+      .expect(403)
+  })
+
+  test('should return 204 when empty list of the surveys with valid access token', async () => {
+    const accessToken = await makeFakeAccount()
+    await request(app)
+      .get('/api/surveys')
+      .set('x-access-token', accessToken)
+      .expect(204)
+  })
+
+  test('should return 200 when list of the surveys with valid access token', async () => {
+    await surveyCollection.insertMany(makeFakeSurveys())
+    const accessToken = await makeFakeAccount()
+    await request(app)
+      .get('/api/surveys')
+      .set('x-access-token', accessToken)
+      .expect(200)
   })
 })
