a7807d7ab6dc1dadba62f59a5636dff76efc77aa
Adriano Santos <adrianodrix@gmail.com>
2021-03-01 22:28:36 -0300

    refactor: remove console logs

--- src/main/server.ts
+++ src/main/server.ts
@@ -2,7 +2,6 @@ import 'module-alias/register'
 import { MongoHelper as mongodb } from '@/infra/db/mongodb/helpers/mongo-helper'
 import env from './config/env'
 
-console.log(env.mongoUrl)
 mongodb.connect(env.mongoUrl)
   .then(async () => {
     const app = (await import('./config/app')).default
