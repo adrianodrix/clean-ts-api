7d32e6f189ed022a707f76f32962b1e93203cc8d
Adriano Santos <adrianodrix@gmail.com>
2021-03-08 23:10:36 -0300

    test: ensure return 200 status code and a id when call LoadSurveyById

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-controller.spec.ts
@@ -3,7 +3,7 @@ import { LoadSurveyById } from '@/domain/usecases/survey/load-survey-by-id'
 import { InvalidParamError, ServerError } from '@/presentation/errors'
 import { HttpRequest } from '../../auth/signup/signup--controller-protocols'
 import { SaveSurveyResultController } from './save-survey-controller'
-import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
+import { forbidden, ok, serverError } from '@/presentation/helpers/http/http-helper'
 
 type SutTypes = {
   sut: SaveSurveyResultController
@@ -62,7 +62,13 @@ describe('SaveSurveyResult Controller', () => {
     expect(httpRespose).toEqual(forbidden(new InvalidParamError('surveyId')))
   })
 
-  test('should return 500 if LoadSurveyById throws', async () => {
+  test('should return 200 status code and a id when call LoadSurveyById', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(ok(makeFakeSurvey()))
+  })
+
+  it('should return 500 if LoadSurveyById throws', async () => {
     const { sut, loadSurveyByIdStub } = makeSut()
     jest.spyOn(loadSurveyByIdStub, 'loadById').mockImplementationOnce(async () => {
       return new Promise((resolve, reject) => reject(new Error()))
