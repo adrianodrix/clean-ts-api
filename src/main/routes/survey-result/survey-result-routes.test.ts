b3865dd93b99d53fd0f27d50d2ccb07c9db049a7
Adriano Santos <adrianodrix@gmail.com>
2021-03-09 00:53:23 -0300

    test: ensure save survey result route return 200 on success

--- src/main/routes/survey-result/survey-result-routes.test.ts
+++ src/main/routes/survey-result/survey-result-routes.test.ts
@@ -3,8 +3,49 @@ import request from 'supertest'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import app from '@/main/config/app'
 import { Collection } from 'mongodb'
+import env from '@/main/config/env'
+import { hash } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+import { SurveyModel } from '@/domain/models/survey'
 
 let surveyCollection, accountCollection: Collection
+
+const makeFakeSurvey = (): SurveyModel => {
+  return {
+    id: 'any_id',
+    question: 'any_question',
+    date: new Date(),
+    answers: [
+      {
+        image: 'any_image',
+        answer: 'any_answer'
+      }
+    ]
+  }
+}
+
+const makeFakeAccount = async (): Promise<string> => {
+  const password = await hash('123', 12)
+  const res = await accountCollection.insertOne({
+    name: 'Adriano Santos',
+    email: 'adrianodrix@gmail.com',
+    role: 'admin',
+    password
+  })
+
+  const id = res.ops[0]._id
+  const accessToken = sign({ id }, env.jwtSecret)
+
+  await accountCollection.updateOne({
+    _id: id
+  }, {
+    $set: {
+      accessToken
+    }
+  })
+
+  return accessToken
+}
 
 describe('SurveyResult Routes', () => {
   afterAll(async () => {
@@ -26,10 +67,24 @@ describe('SurveyResult Routes', () => {
   })
 
   describe('PUT /surveys/:surveyId/results', () => {
-    test('should return 403 on save survey result whithout accessToken', async () => {
+    it('should return 403 on save survey result whithout accessToken', async () => {
       await request(app)
         .put('/api/surveys/any_id/results')
         .expect(403)
+    })
+
+    it('should return 200 on save survey result success', async () => {
+      const accessToken = await makeFakeAccount()
+      const res = await surveyCollection.insertOne(makeFakeSurvey())
+      const survey = MongoHelper.map(res.ops[0])
+
+      await request(app)
+        .put(`/api/surveys/${survey.id}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'any_answer'
+        })
+        .expect(200)
     })
   })
 })
