47ec9794f7f8741a4acf4137e625642c0d96535a
Adriano Santos <adrianodrix@gmail.com>
2021-03-11 17:11:57 -0300

    feat: ensure return 500 if LoadSurveyById throws

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,12 +1,17 @@
 import { LoadSurveyById } from '@/domain/usecases/survey/load-survey-by-id'
-import { ok } from '@/presentation/helpers/http/http-helper'
+import { ServerError } from '@/presentation/errors'
+import { ok, serverError } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class LoadSurveyResultController implements Controller {
   constructor (private readonly loadSurveyById: LoadSurveyById) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { surveyId } = httpRequest.params
-    return ok(await this.loadSurveyById.loadById(surveyId))
+    try {
+      const { surveyId } = httpRequest.params
+      return ok(await this.loadSurveyById.loadById(surveyId))
+    } catch (error) {
+      return serverError(new ServerError(error))
+    }
   }
 }
