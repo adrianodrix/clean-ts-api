a268c120296b81a93c4ded8c56bab663ed4d1c7c
Adriano Santos <adrianodrix@gmail.com>
2021-03-11 17:17:58 -0300

    test: ensure return 403 if LoadSurveyById returns null

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -1,5 +1,6 @@
-import { ServerError } from '@/presentation/errors'
-import { serverError } from '@/presentation/helpers/http/http-helper'
+import { LoadSurveyById } from '@/domain/usecases/survey/load-survey-by-id'
+import { InvalidParamError, ServerError } from '@/presentation/errors'
+import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
 import { HttpRequest } from '@/presentation/protocols'
 import { mockLoadSurveyById } from '@/presentation/test'
 import { LoadSurveyResultController } from './load-survey-result-controller'
@@ -10,7 +11,12 @@ const makeFakeRequest = (): HttpRequest => ({
   }
 })
 
-const makeSut = (): any => {
+type SutTypes = {
+  loadSurveyByIdStub: LoadSurveyById
+  sut: LoadSurveyResultController
+}
+
+const makeSut = (): SutTypes => {
   const loadSurveyByIdStub = mockLoadSurveyById()
   const sut = new LoadSurveyResultController(loadSurveyByIdStub)
   return {
@@ -27,12 +33,19 @@ describe('LoadSurveyResult Controller', () => {
     expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
   })
 
-  it('should return 500 if LoadSurveyById throws', async () => {
+  test('should return 500 if LoadSurveyById throws', async () => {
     const { sut, loadSurveyByIdStub } = makeSut()
     jest.spyOn(loadSurveyByIdStub, 'loadById').mockImplementationOnce(async () => {
       return Promise.reject(new Error())
     })
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new ServerError('')))
+  })
+
+  test('should return 403 if LoadSurveyById returns null', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(Promise.resolve(null))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')))
   })
 })
