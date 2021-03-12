338a9b26d0e57c8ce29f9266a3da8f66cb1eb61a
Adriano Santos <adrianodrix@gmail.com>
2021-03-11 23:25:15 -0300

    feat: ensure return 403 and 200 for SurveyResultController

--- src/main/factories/controllers/survey-result/save-survey-result/save-survey-controller-factory.ts
+++ src/main/factories/controllers/survey-result/save-survey-result/save-survey-controller-factory.ts
@@ -1,8 +1,8 @@
 import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
 import { SaveSurveyResultController } from '@/presentation/controllers/survey-result/save-survey-result/save-survey-controller'
 import { Controller } from '@/presentation/protocols'
-import { makeDbLoadSurveyById } from '../../use-cases/load-survey-by-id/load-survey-by-id-factory'
-import { makeDbSaveSurveyResult } from '../../use-cases/survey-result/db-save-survey-result-factory'
+import { makeDbLoadSurveyById } from '@/main/factories/use-cases/load-survey-by-id/load-survey-by-id-factory'
+import { makeDbSaveSurveyResult } from '@/main/factories/use-cases/survey-result/db-save-survey-result-factory'
 
 export const makeSaveSurveyResultController = (): Controller => {
   const controller = new SaveSurveyResultController(makeDbLoadSurveyById(), makeDbSaveSurveyResult())
